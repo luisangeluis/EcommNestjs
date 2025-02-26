97d8a1d3176bb55b5318dd0520b998e1f1786970
CodeSandbox <support@codesandbox.io>
2025-02-26 19:35:47 +0000

    Sandbox snapshot commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
+//Atajo para crear modules
+//nest g mo users
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
