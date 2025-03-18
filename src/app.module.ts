e3c774ce13a558299329cee4ed45de8f917e552e
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2025-03-18 19:30:11 +0000

    Feature:It was seen how to generate the middlewares and resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule, PaymentsModule],
   controllers: [HelloController],
 })
 export class AppModule {
@@ -26,4 +27,10 @@ export class AppModule {
   //GUARDS
   //Atajo para generar un guard
   //nest g guard /hello/guards/auth
+  //MIDDLEWARES
+  //Atajo para generar un MIDDLEWARE
+  // nest g mi users/logger
+  //RESOURCES
+  //Generar service, controller y module
+  //nest g resource payments
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,37 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //Se usa el middleware en todas las rutas users
+    // consumer.apply(LoggerMiddleware).forRoutes("users");
+
+    //Para usar el middleware en rutas en especifico
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        {
+          path: '/users',
+          method: RequestMethod.GET,
+        },
+        {
+          path: '/users',
+          method: RequestMethod.POST,
+        },
+      )
+      .apply(AuthMiddleware)
+      .forRoutes('users');
+  }
+}
