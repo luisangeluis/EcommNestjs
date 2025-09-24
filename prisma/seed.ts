939aeade32463ba5725833a1e67795092e03f660
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2025-09-24 19:53:31 +0000

    Feature: Some models added to db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,13 +4,22 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+
   for (let i = 0; i < 25; i++) {
     await prisma.product.create({
       data: {
-        id: faker.string.uuid(),
         title: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
         price: faker.commerce.price({ min: 1 }),
+        productImages: {
+          create: Array.from({
+            length: faker.number.int({ min: 1, max: 5 }),
+          }).map(() => ({
+            url: faker.image.urlPicsumPhotos(), // o faker.image.urlLoremFlickr({ category: 'product' })
+          })),
+        },
       },
     });
   }
